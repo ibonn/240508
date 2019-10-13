67f94e5b018378ab34c243289cca61e9b9b6a7ec
Ibon <ibonescartin@gmail.com>
2019-10-13 10:09:25 +0200

    Versión HTML añadida

--- practica3/README.md
+++ practica3/README.md
@@ -1,4 +1,4 @@
 # Práctica 3 - Consultas a la base de datos
-En esta práctica se ha implementado un pequeño sistema que permite consultar una base de datos SQLite. Para ello se ha usado
-Flask. Se pueden consultar los nombres de las tablas de la base de datos y tablas individuales.
-El resultado de las consultas se muestra en JSON, para poder ser usado facilmente por un programa. En la página principal del servidor se encuentran las instrucciones de uso.
+En esta práctica se ha implementado un pequeño sistema que permite consultar una base de datos SQLite. Para ello se ha usado Flask. Se pueden consultar los nombres de las tablas de la base de datos, las columnas de cada tabla y el contenido de tablas individuales.
+Las consultas se pueden realizar mediante una simple interfaz web o la API.
+El resultado de las consultas de la API se muestra en JSON, para poder ser usado facilmente por un programa. En la página principal del servidor se encuentran las instrucciones de uso.
--- practica3/explorer.py
+++ practica3/explorer.py
@@ -1,10 +1,13 @@
+import sys
 import json
+import os.path
 import sqlite3
+
 from flask import Flask, Response, render_template
 
 # Funciones auxiliares
 def lista_tablas():
-    conn = sqlite3.connect('ejemplo.db')
+    conn = sqlite3.connect(sys.argv[1])
     c = conn.cursor()
     c.execute("SELECT * FROM sqlite_master WHERE type='table';")
     tablas = [tabla[1] for tabla in c.fetchall()]
@@ -21,6 +24,30 @@ def mostrar_json(d, s='OK', m=''):
     r.headers["Content-Type"] = 'application/json'
     return r
 
+def mostrar_tabla(table):
+    tablas = lista_tablas()
+    if table in tablas:
+        conn = sqlite3.connect(sys.argv[1])
+        c = conn.cursor()
+        c.execute("SELECT * FROM '{}';".format(table))
+        columnas = [description[0] for description in c.description]
+        tabla = [dict(zip(columnas, r)) for r in c.fetchall()]
+        conn.close()
+        return tabla
+    return None
+
+def mostrar_info_tabla(table):
+    tablas = lista_tablas()
+    if table in tablas:
+        conn = sqlite3.connect(sys.argv[1])
+        c = conn.cursor()
+        c.execute("SELECT * FROM '{}';".format(table))
+        num_rows = len(list(c.fetchall()))
+        columnas = [description[0] for description in c.description]
+        conn.close()
+        return {'columnas': columnas, 'num_registros': num_rows}
+    return None
+
 # Servidor Flask
 app = Flask(__name__)
 
@@ -28,23 +55,43 @@ app = Flask(__name__)
 def mostrar_inicio():
     return render_template('index.html')
 
-@app.route('/tablas')
-def mostrar_tablas():
+@app.route('/tablas/')
+def mostrar_tablas_json():
     return mostrar_json(lista_tablas())
 
-@app.route('/tablas/<table>')
-def mostrar_tabla(table):
-    tablas = lista_tablas()
-    if table in tablas:
-        conn = sqlite3.connect('ejemplo.db')
-        c = conn.cursor()
-        c.execute("SELECT * FROM '{}';".format(table))
-        columnas = [description[0] for description in c.description]
-        tabla = [dict(zip(columnas, r)) for r in c.fetchall()]
-        conn.close()
-        return mostrar_json(tabla)
-    else:
+@app.route('/tablas/<table>/')
+def mostrar_tabla_json(table):
+    tabla = mostrar_tabla(table)
+    if tabla is None:
         return mostrar_json(None, s='error', m='La tabla "{}" no existe'.format(table))
+    else:
+        return mostrar_json(tabla)
+
+@app.route('/tablas/<table>/info/')
+def mostrar_info_tabla_json(table):
+    info = mostrar_info_tabla(table)
+    if info is None:
+        return mostrar_json(None, s='error', m='La tabla "{}" no existe'.format(table))
+    else:
+        return mostrar_json(info)
+
+@app.route('/html/tablas/')
+def mostrar_tablas_html():
+    return render_template('tablas.html', tables=lista_tablas())
+
+@app.route('/html/tablas/<table>/')
+def mostrar_tabla_html(table):
+    return render_template('tabla.html', table=table, data=mostrar_tabla(table))
+
+@app.route('/html/tablas/<table>/info/')
+def mostrar_info_tabla_html(table):
+    return render_template('info_tabla.html', table=table, data=mostrar_info_tabla(table))
 
 if __name__ == '__main__':
-    app.run()
+    if len(sys.argv) < 2:
+        print('Error: Se necesita una base de datos')
+    else:
+        if os.path.isfile(sys.argv[1]):
+            app.run()
+        else:
+            print('Error: No existe el fichero {}'.format(sys.argv[1]))
