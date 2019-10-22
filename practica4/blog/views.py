72949e88cb558bfb7336c68760559e18fd388ce2
Ibon <ibonescartin@gmail.com>
2019-10-22 12:58:06 +0200

    Añadida práctica 4

--- practica4/blog/views.py
+++ practica4/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
+def post_list(request):
+    posts = models.Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
